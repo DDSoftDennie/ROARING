a8a59917ee36a3c028803a9382396afd77c421d7
DDSoftDennie <dennie@ddsoft.be>
2021-09-07 16:07:36 +0200

    Init

---
+++ Roaring/Data/Pictures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Roaring.Data
+{
+    public class Picture
+    {
+        public string Uri { get; set; }
+        public string Alt { get; set; }
+
+        public Picture(string uri, string alt)
+        {
+            Uri = uri;
+            Alt = alt;
+        }
+
+    }
+}
