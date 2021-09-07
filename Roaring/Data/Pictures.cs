49518e35e0b21e670c684e8a1f681419e044651b
DDSoftDennie <dennie@ddsoft.be>
2021-09-07 16:28:42 +0200

    change pictures

--- Roaring/Data/Pictures.cs
+++ Roaring/Data/Pictures.cs
@@ -8,11 +8,30 @@ namespace Roaring.Data
         public string Uri { get; set; }
         public string Alt { get; set; }
 
-        public Picture(string uri, string alt)
+       public Picture(string uri, string alt)
         {
             Uri = uri;
             Alt = alt;
         }
 
+    }
+
+    public class Pictures{
+        private List<Picture> _pictures {get; set;}
+
+        Pictures()
+        {
+            _pictures = new List<Picture>();
+        }
+
+        public void AddPicture(string Uri, string Alt)
+        {
+            _pictures.Add(new Picture(Uri, Alt));
+        }
+
+        public List<Picture> GetAllPictures()
+        {
+            return _pictures;
+        }
     }
 }
